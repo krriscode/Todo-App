f1d30556756e310f2532283c2c7c00c8eb34bf3e
kris <krris.pratap@gmail.com>
2025-07-22 10:40:24 +0530

    Added checkbox

--- src/Components/AppOne.jsx
+++ src/Components/AppOne.jsx
@@ -15,7 +15,7 @@ export default function AppOne() {
 
   const editList = (index) => {
     let newArr = [...arrList];
-    newArr[index].editMode = true;
+    newArr[index].editMode = !newArr[index].editMode;
     setArrList(newArr);
     setCurrentValue(newArr[index].data);
   }
@@ -23,10 +23,16 @@ export default function AppOne() {
   const handleBlur = (index) => {
     const newList = [...arrList];
     newList[index].data = currentValue;
-    newList[index].editMode = false;
+    newList[index].editMode = !newList[index].editMode;
     setArrList(newList);
     setCurrentValue(null);
   };
+
+  const handleCheckBox = (e, index) => {
+    const tempList = [...arrList];
+    tempList[index].check = e.target.checked;
+    setArrList(tempList);
+  }
 
   return (
     <div className={styles.container}>
@@ -37,13 +43,18 @@ export default function AppOne() {
 
       <div className={styles.section_nav}>
         <input type="text" value={temp} placeholder="Add New Task" onChange={(e => setTemp(e.target.value))} />
-        <input type="submit" value="Add" onClick={() => { setArrList([...arrList, { data: temp, editMode: false }]); setTemp(""); }} />
+        <input type="submit" value="Add" onClick={() => { setArrList([...arrList, { data: temp, editMode: false, check: false }]); setTemp(""); }} />
       </div>
 
       {(arrList.length > 0) && <div className={styles.section_list}>
         {arrList.map((val, index) => (
           <div key={index}>
-            {!val.editMode && <label htmlFor="" className={styles.item_value}>{val.data}</label>}
+            {!val.editMode && (
+              <>
+                <input type="checkbox" id={`check${index}`} style={{ marginRight:"30px" }} checked={val.check} onChange={e => handleCheckBox(e, index)} />
+                <label htmlFor={`check${index}`} style={{ textDecoration: val.check ? "line-through" : "none", marginRight:"20px" }} className={styles.item_value}>{val.data}</label>
+              </>
+            )}
             {val.editMode && <input type="text" value={currentValue} onChange={(e) => setCurrentValue(e.target.value)} onBlur={() => handleBlur(index)} autoFocus />}
             <button className={`${styles.btn} icon-e`} onClick={() => editList(index)}>Edit</button>
             <button className={`${styles.btn} icon-d`} onClick={() => delList(index)}>Delete</button>
